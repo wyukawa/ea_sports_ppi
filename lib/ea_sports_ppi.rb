c32dfc1b266a7c8febd4acfa5fe5a8b580c29097
wyukawa <wyukawa@gmail.com>
2013-04-29 13:48:40 +0000

    parse web page using nokogiri html parser

--- lib/ea_sports_ppi.rb
+++ lib/ea_sports_ppi.rb
@@ -1,5 +1,15 @@
 require "ea_sports_ppi/version"
 
+require 'nokogiri'
+require 'open-uri'
+
 module EaSportsPpi
   # Your code goes here...
+  def self.run()
+    doc=Nokogiri::HTML(open("http://www.premierleague.com/en-gb/players/ea-sports-player-performance-index/"))
+    tables=doc.xpath('/html/body/div/div/div/div/div/div/div/form/div/table')
+    tables[0].xpath('tbody/tr').each do |tr|
+      print(tr.xpath('td[4]').inner_text.strip, " ", tr.xpath('td[14]').inner_text.strip, "\n")
+    end
+  end
 end
